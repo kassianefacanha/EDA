c53ca1a3683bd6b716244f669f0796ba8d0a3503
Kassiane Façanha <kassianelf@alu.ufc.br>
2021-09-02 22:42:58 -0300

    ultimos testes nada certo

--- Tópico 10 - Grafos/Q1/main.cpp
+++ Tópico 10 - Grafos/Q1/main.cpp
@@ -10,7 +10,7 @@ int main()
 {
 	DisjointSets p;
 	
-	int itr = 0;
+    int itr = 0;
 	while(true) {
 		string line, word;
 		getline(cin, line);
@@ -26,22 +26,16 @@ int main()
 			p.makeset( std::stoi(tokens[i]) );	
 		
 		// unions
-		if(itr >= 12){
-			int u1 = std::stoi(tokens[1]);
-			int u2 = std::stoi(tokens[2]);
-			p.unionSets(u1,u2);		    
-			
-			// finds
-    		int e1 = std::stoi(tokens[1]);
-    		int e2 = std::stoi(tokens[2]);
-    		if(p.find(e1) == p.find(e2)) 
-    			cout << " SIM" << endl;
-    		else 
-    			cout<< " NAO" << endl;
-		}
-		
-		itr++;
-		
+		int u1 = std::stoi(tokens[1]);
+		int u2 = std::stoi(tokens[2]);
+		p.unionSets(u1,u2);		    
+
+		int e1 = std::stoi(tokens[1]);
+		int e2 = std::stoi(tokens[2]);
+		if(p.find(e1) == p.find(e2)) 
+			cout << e1 << " e " << e2 << " estao no mesmo conjunto" << endl;
+		else 
+			cout << e1 << " e " << e2 << " estao em conjuntos disjuntos" << endl;			
 	}
 
 	return 0;
